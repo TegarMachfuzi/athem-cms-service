b5f3168c44a7842c51f51edb618cedebad7fe78e
TegarMachfuzi <tegarmachfudzi99@gmail.com>
2024-08-08 14:28:32 +0700

    first commit

---
+++ src/test/java/com/athem/cms/service/cms_service/CmsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athem.cms.service.cms_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
